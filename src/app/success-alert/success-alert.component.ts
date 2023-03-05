b42f9bda81bd02cf5acade006b13680d7b19133f
eermakov <EVErmakov@lanit.ru>
2023-03-05 14:51:17 +0300

    Basics: 26. String Interpolation

---
+++ src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-success-alert',
+  templateUrl: './success-alert.component.html',
+  styleUrls: ['./success-alert.component.css']
+})
+export class SuccessAlertComponent {
+  serverId: number = 10;
+  serverStatus: string = 'offline';
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+}
