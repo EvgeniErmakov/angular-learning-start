840b3549228e12f7f915f44f6fbc3fd1a82bee96
eermakov <EVErmakov@lanit.ru>
2023-03-05 12:53:50 +0300

    Basics: add a way to add css-styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css']
+  styles: [
+    `div { color :orangered; font-size: 36px}`
+  ]
 })
 export class AppComponent {
 }
