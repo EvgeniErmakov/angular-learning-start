8cfed424f9b0487cea091d049e59548dd20d7372
eermakov <EVErmakov@lanit.ru>
2023-03-05 13:15:00 +0300

    BASICS: 23. Fully Understanding the Component Selector

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
+  //   selector: '[app-servers]', // тогда можно будет использовать в app.component.html через <div app-servers></div>
+  // selector: '.app-servers',  // тогда можно будет использовать в app.component.html через <div class="app-servers"></div>
   selector: 'app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
