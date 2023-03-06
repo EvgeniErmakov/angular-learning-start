9aa62eda3421c09710d8b9ea3e0f4834a996b2cb
eermakov <EVErmakov@lanit.ru>
2023-03-06 20:19:41 +0300

    BASICS: 41. Outputting Lists with ngFor

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,9 +1,19 @@
-import { Component } from "@angular/core";
+import {Component} from "@angular/core";
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html'
 })
 export class ServerComponent {
+  number: number = 0;
+  serverList = ['server1','server2','server3'];
 
+  constructor() {
+    this.number = this.serverList.length;
+  }
+
+  addServer() {
+    this.number++;
+    this.serverList.push("server" + this.number);
+  }
 }
