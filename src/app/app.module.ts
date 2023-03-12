b0eea27f8dfecff57bc07bb7c35e8a7a97a905a9
eermakov <EVErmakov@lanit.ru>
2023-03-12 23:13:50 +0300

    94.Using the Renderer to build a Better Attribute Directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { FormsModule } from "@angular/forms";
-import { CockpitComponent } from './cockpit/cockpit.component';
-import { ServerElementComponent } from './server-element/server-element.component';
-import { GameControlComponent } from './game-control/game-control.component';
-import { OddComponent } from './odd/odd.component';
-import { EvenComponent } from './even/even.component';
+import {AppComponent} from './app.component';
+import {FormsModule} from "@angular/forms";
+import {CockpitComponent} from './cockpit/cockpit.component';
+import {ServerElementComponent} from './server-element/server-element.component';
+import {GameControlComponent} from './game-control/game-control.component';
+import {OddComponent} from './odd/odd.component';
+import {EvenComponent} from './even/even.component';
+import {BasicHighlightDirective} from "./basic-highlight/basic-highlight.directive";
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { EvenComponent } from './even/even.component';
     ServerElementComponent,
     GameControlComponent,
     OddComponent,
-    EvenComponent
+    EvenComponent,
+    BasicHighlightDirective
   ],
   imports: [
     BrowserModule,
