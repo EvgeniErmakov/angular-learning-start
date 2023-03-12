aecf52be8ed858a333dd4d2ca8a3cdad16a9e9a8
eermakov <EVErmakov@lanit.ru>
2023-03-12 23:19:36 +0300

    94.Using the Renderer to build a Better Attribute Directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import {GameControlComponent} from './game-control/game-control.component';
 import {OddComponent} from './odd/odd.component';
 import {EvenComponent} from './even/even.component';
 import {BasicHighlightDirective} from "./basic-highlight/basic-highlight.directive";
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import {BasicHighlightDirective} from "./basic-highlight/basic-highlight.directi
     GameControlComponent,
     OddComponent,
     EvenComponent,
-    BasicHighlightDirective
+    BasicHighlightDirective,
+    BetterHighlightDirective
   ],
   imports: [
     BrowserModule,
