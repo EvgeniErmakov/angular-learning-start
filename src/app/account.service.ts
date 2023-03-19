27d27d2e59b04cc8baa3870b57857815bf99c971
eermakov <EVErmakov@lanit.ru>
2023-03-19 10:24:40 +0300

    Service: 111.Injecting Services into Services

--- src/app/account.service.ts
+++ src/app/account.service.ts
@@ -1,4 +1,11 @@
+import {Injectable} from "@angular/core";
+import {LoggingSevice} from "./logging.sevice";
+
+@Injectable()
 export class AccountService {
+
+  constructor(private loggingService: LoggingSevice) {}
+
   accounts = [
     {
       name: 'Master Account',
@@ -16,9 +23,11 @@ export class AccountService {
 
   addAccount(name: string, status: string) {
     this.accounts.push({name: name, status: status});
+    this.loggingService.logStatusChange(status);
   }
 
   updateStatus(id: number, status: string) {
     this.accounts[id].status = status;
+    this.loggingService.logStatusChange(status);
   }
 }
--- src/app/logging.sevice.ts
+++ src/app/logging.sevice.ts
@@ -1,3 +1,6 @@
+import {Injectable} from "@angular/core";
+
+@Injectable()
 export class LoggingSevice {
   logStatusChange(status: string) {
     console.log("Status has been changed -> " + status)
