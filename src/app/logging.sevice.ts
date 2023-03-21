6234267a306d54d97cda20486fffd969e76fa518
eermakov <EVErmakov@lanit.ru>
2023-03-21 21:36:06 +0300

    Routing: init

--- src/app/logging.sevice.ts
+++
@@ -1,8 +0,0 @@
-import {Injectable} from "@angular/core";
-
-@Injectable()
-export class LoggingSevice {
-  logStatusChange(status: string) {
-    console.log("Status has been changed -> " + status)
-  }
-}
