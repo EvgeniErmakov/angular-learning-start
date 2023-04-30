3d0370df61644bac56374fed7574326016dfdb13
Evgeni Ermakov <EVErmakov@lanit.ru>
2023-04-30 19:27:03 +0300

    Observables: 175. Errors & Completion

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,32 +9,42 @@ import {interval, Observable, Subscription} from "rxjs";
 export class HomeComponent implements OnInit, OnDestroy {
 
   private firstObsSubscription: Subscription;
-  private firstObsSubscription2: Subscription;
-  constructor() { }
+
+  constructor() {
+  }
 
   ngOnInit() {
-   // this.firstObsSubscription = interval(1000).subscribe(count => {
+    // this.firstObsSubscription = interval(1000).subscribe(count => {
     //  console.log(count);
-   // });
+    // });
 
     const customIntervalObservable = new Observable<number>(observer => {
       let count = 0;
       setInterval(() => {
         observer.next(count);
+        if (count === 2) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error('Count is greater 3!'));
+        }
         count++;
       }, 1000);
     });
 
     this.firstObsSubscription = customIntervalObservable.subscribe(data => {
-      console.log(data);
-    });
-    this.firstObsSubscription2 = customIntervalObservable.subscribe(data => {
-      console.log("The second one = " + data);
-    });
+        console.log(data);
+      },
+      error => {
+        console.log(error);
+        alert(error.message);
+      },
+      () => {
+        console.log('Completed');
+      });
   }
 
   ngOnDestroy() {
     this.firstObsSubscription.unsubscribe();
-    this.firstObsSubscription2.unsubscribe()
   }
 }
