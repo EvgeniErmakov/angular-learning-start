8a6d6b995803a07cbf340fc67963e4e086b0e13f
Evgeni Ermakov <EVErmakov@lanit.ru>
2023-05-04 22:01:08 +0300

    HTTP: 267. Using the catchError Operator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,36 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Post} from './post.model'
 import {PostsService} from "./posts.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   loadedPosts: Post[] = [];
   isFetching = false;
   error = null;
+  private errorSub: Subscription;
 
   constructor(private http: HttpClient, private postsService: PostsService) {
   }
 
   ngOnInit() {
-    this.fetchPosts();
+    this.errorSub = this.postsService.error.subscribe(error => {
+      this.error = error;
+    });
+    this.fetchPosts2();
   }
 
   onCreatePost(postData: { title: string; content: string }) {
-    this.postsService.createAndStorePost(postData.title, postData.content)
-      .subscribe(responseData => {
-        console.log(responseData);
-      });
-    console.log(postData);
+    this.postsService.createAndStorePost(postData.title, postData.content);
   }
 
   onFetchPosts() {
-    this.fetchPosts();
+    this.fetchPosts2();
   }
 
   onClearPosts() {
@@ -66,5 +67,9 @@ export class AppComponent implements OnInit {
           this.error = handledError.message;
         }
       });
+  }
+
+  ngOnDestroy() {
+    this.errorSub.unsubscribe();
   }
 }
--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,33 +1,45 @@
 import {Post} from "./post.model";
-import {map} from "rxjs";
+import {catchError, map, Subject, throwError} from "rxjs";
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
+  error = new Subject<String>();
+
 
   constructor(private http: HttpClient) {
   }
 
   createAndStorePost(title: string, content: string) {
     const postData: Post = {title: title, content: content};
-    return this.http
+    this.http
       .post<{ name: string }>
-      ('https://ng-complete-guide-63cf7-default-rtdb.firebaseio.com/posts.json', postData);
+      ('https://ng-complete-guide-63cf7-default-rtdb.firebaseio.com/posts.json', postData)
+      .subscribe(responseData => {
+          console.log(responseData);
+        },
+        error => {
+          this.error.next(error.message());
+        });
   }
 
   fetchPosts() {
     return this.http
       .get<{ [key: string]: Post }>('https://ng-complete-guide-63cf7-default-rtdb.firebaseio.com/posts.json')
       .pipe(map(responseData => {
-        const postArray: Post[] = [];
-        for (const key in responseData) {
-          if (responseData.hasOwnProperty(key)) {
-            postArray.push({...responseData[key], id: key});
+          const postArray: Post[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postArray.push({...responseData[key], id: key});
+            }
           }
-        }
-        return postArray;
-      }))
+          return postArray;
+        }),
+        catchError(err => {
+          console.log('словили ошибочку')
+          return throwError(err);
+        }))
   }
 
   clearPost() {
