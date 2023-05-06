522b995920c5002c6a8149aeef9312401c484b39
Evgeni Ermakov <EVErmakov@lanit.ru>
2023-05-06 13:46:12 +0300

    HTTP: 274. Manipulating Request Objects

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -4,6 +4,12 @@ export class AuthInterceptorService implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     console.log("Перед тем как запросик ушел, вызвансля перехватчик-Interceptor!")
-    return next.handle(req);
+    // принимаемый запрос является имьютабельным (неизменяемым), мы не может в него что-либо добавить, но можем копировать
+    // и добавить что-то своё в скопированный запрос. Далее, мы можем в next.handle(out new request) передать и отправить запрос!
+    // Пример:
+    const modifiedRequest = req.clone({
+      headers: req.headers.append('NewHeader','Header was added by Interceptors')
+    })
+    return next.handle(modifiedRequest);
   }
 }
