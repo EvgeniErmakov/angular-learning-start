c0f59ee0717f03eaf07ae4030d493501135766a7
Evgeni Ermakov <EVErmakov@lanit.ru>
2023-05-06 16:26:40 +0300

    Pipes: 246. Creating a Custom Pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
-
 import { AppComponent } from './app.component';
+import {ShortPipe} from "./short.pipe";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShortPipe
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
