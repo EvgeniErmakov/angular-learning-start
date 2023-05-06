ae667f359dbde2d7fc4066369137863402299882
Evgeni Ermakov <EVErmakov@lanit.ru>
2023-05-06 13:06:23 +0300

    HTTP: 271. Observing Different Types of Responses

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,7 +1,8 @@
 import {Post} from "./post.model";
-import {catchError, map, Subject, throwError} from "rxjs";
+import {catchError, map, Subject, tap, throwError} from "rxjs";
 import {Injectable} from "@angular/core";
-import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
+import {HttpClient, HttpEventType, HttpHeaders, HttpParams} from "@angular/common/http";
+import * as events from "events";
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
@@ -15,7 +16,13 @@ export class PostsService {
     const postData: Post = {title: title, content: content};
     this.http
       .post<{ name: string }>
-      ('https://ng-complete-guide-63cf7-default-rtdb.firebaseio.com/posts.json', postData)
+      ('https://ng-complete-guide-63cf7-default-rtdb.firebaseio.com/posts.json',
+        postData,
+        //По умолчанию HttpClient возвращает тело ответа. Чтобы получить полный ответ, можно передать объект с ключом observe, которому присвоено значение ‘response’.
+        {
+          observe: 'response'
+        }
+      )
       .subscribe(responseData => {
           console.log(responseData);
         },
@@ -30,7 +37,7 @@ export class PostsService {
         {
           headers: new HttpHeaders({'Custom-Header': 'Hello!!'}),
           // Можно задать параметры через конструктор, как на следующей строке
-         // params: new HttpParams().set('key!','value!')
+          // params: new HttpParams().set('key!','value!')
           params: new HttpParams()
             .set('firstOne', 'value1')
             .set('secondOne', 'value2')
@@ -51,6 +58,20 @@ export class PostsService {
   }
 
   clearPost() {
-    return this.http.delete('https://ng-complete-guide-63cf7-default-rtdb.firebaseio.com/posts.json');
+    return this.http.delete('https://ng-complete-guide-63cf7-default-rtdb.firebaseio.com/posts.json',
+      {
+        observe: 'events'
+      }
+    ).pipe(
+      tap(event => {
+        console.log(event)
+        if (event.type === HttpEventType.Sent) {
+          // ...
+        }
+
+        if (event.type === HttpEventType.Response) {
+          console.log('body -> ' + event.body);
+        }
+      }));
   }
 }
