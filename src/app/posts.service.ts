d01e6189b14066beaa2a7293e5ae26ab34bc99da
Evgeni Ermakov <EVErmakov@lanit.ru>
2023-05-06 12:11:51 +0300

    HTTP: 270. Adding Query Params

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,7 +1,7 @@
 import {Post} from "./post.model";
 import {catchError, map, Subject, throwError} from "rxjs";
 import {Injectable} from "@angular/core";
-import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
@@ -28,7 +28,12 @@ export class PostsService {
     return this.http
       .get<{ [key: string]: Post }>('https://ng-complete-guide-63cf7-default-rtdb.firebaseio.com/posts.json',
         {
-          headers: new HttpHeaders({'Custom-Header': 'Hello!!'})
+          headers: new HttpHeaders({'Custom-Header': 'Hello!!'}),
+          // Можно задать параметры через конструктор, как на следующей строке
+         // params: new HttpParams().set('key!','value!')
+          params: new HttpParams()
+            .set('firstOne', 'value1')
+            .set('secondOne', 'value2')
         })
       .pipe(map(responseData => {
           const postArray: Post[] = [];
