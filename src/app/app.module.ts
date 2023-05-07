ad577fa65079461c2d1a350ceec849ea9d9fe68f
Evgeni Ermakov <EVErmakov@lanit.ru>
2023-05-07 11:15:32 +0300

    Pipes: 248. Example: Creating a Filter Pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import {ShortPipe} from "./short.pipe";
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShortPipe
+    ShortPipe,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
