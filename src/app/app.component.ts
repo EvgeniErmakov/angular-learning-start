140a657b4e1959b1daaee8b8f9033c6a3b7937c0
Evgeni Ermakov <EVErmakov@lanit.ru>
2023-05-07 17:32:39 +0300

    Forms: init the branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,12 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {UserService} from "./user.service";
-import {Subscription} from "rxjs";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  private subscription: Subscription;
-  userActivated = false;
-
-  constructor(private userService: UserService) {}
-
-  ngOnInit() {
-    let subscription = this.userService.activatedEmitter.subscribe(didActivate => {
-      this.userActivated = didActivate;
-    });
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
+export class AppComponent {
+  suggestUserName() {
+    const suggestedName = 'Superuser';
   }
 }
