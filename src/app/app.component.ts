0efdd6e670cd82d162482bbb103e56acbcf34b7d
Evgeni Ermakov <EVErmakov@lanit.ru>
2023-05-08 12:39:26 +0300

    Forms: 200. TD: Using Form Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,15 @@ export class AppComponent {
   @ViewChild('f') signUpForm: NgForm;
   answer = '';
   genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion:'',
+    answer:'',
+    gender: ''
+  }
+  submitted = false;
+
 
   suggestUserName() {
     const suggestedName = 'Superuser';
@@ -42,6 +51,13 @@ export class AppComponent {
    */
 
   onSubmit() {
-    console.log(this.signUpForm)
+    console.log(this.signUpForm);
+    this.user.username = this.signUpForm.value.userData.username;
+    this.user.email = this.signUpForm.value.userData.email2;
+    this.user.secretQuestion = this.signUpForm.value.secret;
+    this.user.answer = this.signUpForm.value.questionAnswer;
+    this.user.gender = this.signUpForm.value.gender;
+
+    this.submitted = true;
   }
 }
